8d4d4968c028c1fedab16660a7c4d7e6a88d3f9a
Emlyn Corrin <emlyn@swiftkey.net>
2013-01-28 14:49:44 +0000

    info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject mrs-doyle-jr "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject mrs-doyle-jr "0.1.0"
+  :description "Port of Mrs Doyle (https://AdamClements/MrsDoyle) to clojure"
+  :url "https://github.com/emlyn/MrsDoyle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
